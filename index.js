df8c24296b78715dc415c71d13d4cde234416032
FerdousFardin <fardinh39u@gmaile.com>
2022-12-29 20:12:31 +0600

    api done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -13,6 +13,38 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+async function run() {
+  try {
+    const database = client.db("task-manager");
+    const tasksCollection = database.collection("tasks");
+
+    app.get("/tasks", async (req, res) => {
+      const tasks = await tasksCollection.find({}).sort({ date: -1 }).toArray();
+      res.send(tasks);
+    });
+    app.post("/tasks", async (req, res) => {
+      const tasksBody = req.body;
+      const results = await tasksCollection.insertOne(tasksBody);
+      res.send(results);
+    });
+    app.put("/tasks", async (req, res) => {
+      const updatedTask = req.body;
+      const { id, ...rest } = req.body;
+      const filter = { _id: ObjectId(id), rest };
+      const results = await tasksCollection.updateOne(filter, updatedTask, {
+        upsert: true,
+      });
+      res.send(results);
+      app.delete("/tasks", async (req, res) => {
+        const { id, ...rest } = req.body;
+        const filter = { _id: ObjectId(id), rest };
+        const results = await tasksCollection.deleteOne(filter);
+      });
+    });
+  } finally {
+  }
+}
+run().catch((er) => console.error(er));
 
 app.get("/", (req, res) => {
   res.send("Server responded");
